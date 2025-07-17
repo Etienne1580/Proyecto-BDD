2cb79ae9948dc643f8ec37e4ddc9093c035c01e1
eagd2 <eagd2@DESKTOP-QK1VKRI>
2025-07-16 22:17:05 -0700

    Primer aporte

---
+++ src/proyecto/bdd/ProyectoBDD.java
@@ -0,0 +1,14 @@
+
+package proyecto.bdd;
+
+
+public class ProyectoBDD {
+
+
+    public static void main(String[] args) {
+        
+        FLogin Login = new FLogin();
+        Login.setVisible(true);
+    }
+    
+}
