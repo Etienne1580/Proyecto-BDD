6b8a3975a7b00e60a4d6141cd00ea1b81ccf2d50
eagd2 <eagd2@DESKTOP-QK1VKRI>
2025-08-02 22:05:13 -0700

    Codigo completamente funcional

--- src/proyecto/bdd/MariaDB.java
+++ src/proyecto/bdd/MariaDB.java
@@ -23,8 +23,8 @@ public class MariaDB {
     public MariaDB() {
         //inicializamos los necsario para conectarnos
         url = "jdbc:mariadb://localhost/ordenes";
-        usuario = "usr";
-        clave = "1234";
+        usuario = "root";
+        clave = "";
 
         try {
             conn = DriverManager.getConnection(url, usuario, clave);
